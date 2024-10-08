08defafa2241241a008f08964bf9e044674f1159
Iago Gomes <iagoomes@outlook.com>
2024-10-08 19:56:10 -0300

    feat: initial commit

---
+++ src/test/java/com/grupo7/parkingmeter/ParkingMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.parkingmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
